4ed631af11d75fa867530d640d09852495c5bb5b
Mike <mikedcryptocurrency@gmail.com>
2023-03-17 18:03:34 -0500

    adjusted max_safe_number to 8999999999999999999

--- constants.hpp
+++ constants.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-const uint64_t MAX_SAFE_NUMBER = 18446744073709551615;
+const uint64_t MAX_SAFE_NUMBER = 8999999999999999999;
 
 const std::vector<uint8_t> LOCK_STATUSES = {
 	0, //Created, awaiting deposit
